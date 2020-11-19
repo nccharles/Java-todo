3e6c417d3701d6d74f1c455e4897317f487dc234
Charles NDAYISABA <nccharles1@gmail.com>
2020-11-19 15:49:43 +0200

    database integration

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -2,11 +2,11 @@ package models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Task {
-
-    private String description;
     private static ArrayList<Task> instances = new ArrayList<>();
+    private String description;
     private boolean completed;
     private LocalDateTime createdAt;
     private int id;
@@ -19,16 +19,35 @@ public class Task {
         this.id = instances.size();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task)) return false;
+        Task task = (Task) o;
+        return getCompleted() == task.getCompleted() &&
+                getId() == task.getId() &&
+                Objects.equals(getDescription(), task.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDescription(), getCompleted(), getId());
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
-    }
-
-    public static ArrayList<Task> getAll(){
-        return instances;
-    }
-
-    public static void clearAllTasks(){
-        instances.clear();
     }
 
     public boolean getCompleted(){
@@ -42,7 +61,13 @@ public class Task {
     public int getId() {
         return id;
     }
+    public static ArrayList<Task> getAll(){
+        return instances;
+    }
 
+    public static void clearAllTasks(){
+        instances.clear();
+    }
     public static Task findById(int id){
         return instances.get(id-1); //why minus 1? See if you can figure it out.
     }
